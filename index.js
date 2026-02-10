7e13216004be488637fedf8156e7763821db99de
Matthew Swanberg <matt20swanberg@gmail.com>
2026-02-10 17:43:04 -0500

    Added attendee object and functions

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 //Write your code here
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+}
 
+// This function takes the attendee object as a parameter and logs the name
+function logAttendeeName(attendee) {
+  console.log(attendee.name);
+}
 
+// This function takes the attendee object as a parameter and logs the ticket price
+function logTicketPrice(attendee) {
+  console.log(attendee.ticketPrice)
+}
+
+// This function takes the attendee object as a parameter and updates the ticket type to a new ticket type
+function updateTicketType(attendee, ticketType) {
+  return attendee.ticketType = ticketType;
+}
+
+// This function takes the attendee object as a parameter and updates the ticket price to a new ticket price
+function updateTicketPrice(attendee, ticketPrice) {
+  return attendee.ticketPrice = ticketPrice;
+}
+
+// This function takes the attendee object as a parameter and removes (i.e. deletes) the event property
+function removeEventProperty(attendee) {
+  delete attendee.event;
+}
+
+// Create a function named addCheckedInProperty that takes the attendee object as a parameter
+// and adds a new property checkedIn with the value true
+function addCheckedInProperty(attendee){
+  attendee.checkedIn = true;
+}
 
 //Needed for the tests to work. Don't modify
 module.exports = {
